0b2244e45bfe25c7c702283383865d08e597bbe6
Brutus Bonginkosi <bbn.ntuli@gmail.com>
2023-01-05 14:42:27 -0800

    eddited lists.h

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -2,33 +2,21 @@
 
 #define LISTS_H
 
+
+
 #include <stdlib.h>
 
 
 
-/**
- *
- *  * struct listint_s - singly linked list
- *
- *   * @n: integer
- *
- *    * @next: points to the next node
- *
- *     *
- *
- *      * Description: singly linked list node structure
- *
- *       * for Holberton project
- *
- *        */
+
 
 typedef struct listint_s
 
 {
 
-	    int n;
+		int n;
 
-	        struct listint_s *next;
+			struct listint_s *next;
 
 } listint_t;
 
