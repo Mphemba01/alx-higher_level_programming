1a9abb104af62e1bf0e5a4b578eba53ca5ed4b89
Brutus Bonginkosi <bbn.ntuli@gmail.com>
2023-01-06 15:04:18 -0800

    task 14 added

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-for i in range(25, -1, -1):
-        c = i + ord('A')
-            if i % 2 == 1:
-                        c += 32
-                            print("{:c}".format(c), end="")
+for i in range(ord('z'), ord('a') - 1, -1):
+    if i % 2 == 0:
+        diff = 0
+    else:
+        diff = 32
+    print('{}'.format(chr(i - diff)), end='')
